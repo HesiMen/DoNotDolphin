a39eafe1b8c137dab8261b39ac7c6700ebea6c2e
isseeeeei <56749460+isseeeeei@users.noreply.github.com>
2019-11-18 17:40:46 -0800

    adding model, material
    go

---
+++ DoNotDolphin/Assets/Scripts/Auto_moving_airplane.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Auto_moving_airplane : MonoBehaviour
+{
+    public float speed = 0.1f;
+    public GameObject[] children;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        transform.Rotate(new Vector3(0, Time.deltaTime / 2 * -speed, 0));
+
+        foreach (var child in children)
+        {
+            child.transform.Rotate(new Vector3(0, Time.deltaTime / 2 * -speed, 0));
+        }
+}
+}
