06566c5410d414ce9ae7ee01a212e1f750fe5e67
HesiMen <hesi.mendez@gmail.com>
2019-11-19 11:46:24 -0800

    Triggers and Shader

---
+++ DoNotDolphin/Assets/Scripts/Script_SoundTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_SoundTrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] AudioSource warpSound;
+ 
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            warpSound.loop = true;
+            warpSound.Play();
+        }
+    }
+}
